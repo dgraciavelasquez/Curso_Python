7ee3085df6607e6ece8a23545835a26ac22d7e72
dgraciavelasquez <davidgraciaru@gmail.com>
2022-10-25 11:50:07 -0500

    Cambio en juego del ahorcado

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -1,12 +1,94 @@
-def read():
-    palabras = []
-    with open(".\archivos\numbers.txt", "r", encoding="utf-8") as f:
-        for line in f:
-            palabras.append((line))
-    print(palabras)
-
+import random
+import os
+from tkinter.tix import IMAGE
 def run():
-    read()
+    IMAGE = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+    words=["crema",
+    "cafe",
+    "estrella",
+    "explosion",
+    "guitarra"]
+    word = random.choice(words)
+    spaces = ["_"] * len(word)
+    attemps = 6
+
+    while True:
+        os.system("clear")
+        for character in spaces:
+            print(character, end=" ")
+        print(IMAGE[attemps])
+        letter = input(" elige una letra")
+
+        found = False
+        for idx, character in enumerate(word):
+            if character == letter:
+                spaces[idx] = letter
+                found = True
+        
+        if not found:
+            attemps -=1
+
+        if "_" not in spaces:
+            os.system("clear")
+            print("Ganaste")
+            break
+            input()
+
+        if attemps == 0:
+            os.system("clear")
+            print("Perdiste")
+            break
+            input()
 
 if __name__ == "__main__":
     run()
